1f6b633ea18ae3cfd37cf03439dfb8237ed2c526
alexrf45 <fonalex45@gmail.com>
2024-11-29 12:22:49 -0500

    resume dev

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh-keygen -t ed25519 -N '' -C "f0zny" -f r-ssh
+ssh-keygen -t ed25519 -N '' -C "fr3d" -f r-ssh
 
 RS_SHELL="/bin/bash" RS_PASS="$1" RS_PUB="$(cat r-ssh.pub)" make compressed
